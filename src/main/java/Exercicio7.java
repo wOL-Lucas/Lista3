fe10a3eb2f6a09b603778352b48451f8916da8a7
wOL-Lucas <lucasefernandes333@gmail.com>
2023-09-22 01:08:40 -0300

    finished exercicio7

---
+++ src/main/java/Exercicio7.java
@@ -0,0 +1,18 @@
+/*
+Foi realizada uma pesquisa em Niterói, com um número desconhecido de pessoas. De cada entrevistado foram colhidos os seguintes dados:
+1. Qual o seu clube de futebol de preferência (1 – Flamengo, 2 – Vasco, 3 – Fluminense, 4 – Botafogo, 5 – Outros)
+2. Qual o seu salário
+3. Qual a sua cidade natal (1 – Niterói, 2 – Outra)
+Escreva um programa que informe:
+• Número de torcedores por clube
+• Média salarial dos torcedores de cada time
+• Número de pessoas nascidas em Niterói e que não torcem para nenhum dos principais
+   clubes do Rio
+• Número de pessoas entrevistadas
+*/
+public class Exercicio7 {
+    public static void main(String [] args){
+        // do stuff
+    }
+
+}
